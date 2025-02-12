3f4670644ab6cb773b8aaca79347655d6f8d4f5a
Kyle Johnson <kyle@powerworld.com>
2025-02-12 16:16:22 -0600

    Add project files.

---
+++ SimAuto Example C# Core/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace SimAuto_Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnConnect = new System.Windows.Forms.Button();
+            btnDisconnect = new System.Windows.Forms.Button();
+            textCaseFileName = new System.Windows.Forms.TextBox();
+            btnOpenCase = new System.Windows.Forms.Button();
+            btnCloseCase = new System.Windows.Forms.Button();
+            btnGetGenParams = new System.Windows.Forms.Button();
+            btnOPF = new System.Windows.Forms.Button();
+            btnScaleCase = new System.Windows.Forms.Button();
+            btnSendGenToExcel = new System.Windows.Forms.Button();
+            gbOperations = new System.Windows.Forms.GroupBox();
+            textLog = new System.Windows.Forms.TextBox();
+            gbOperations.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            btnConnect.Location = new System.Drawing.Point(14, 14);
+            btnConnect.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            btnConnect.Name = "btnConnect";
+            btnConnect.Size = new System.Drawing.Size(88, 27);
+            btnConnect.TabIndex = 0;
+            btnConnect.Text = "Connect";
+            btnConnect.UseVisualStyleBackColor = true;
+            btnConnect.Click += btnConnect_Click;
+            // 
+            // btnDisconnect
+            // 
+            btnDisconnect.Enabled = false;
+            btnDisconnect.Location = new System.Drawing.Point(108, 14);
+            btnDisconnect.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            btnDisconnect.Name = "btnDisconnect";
+            btnDisconnect.Size = new System.Drawing.Size(88, 27);
+            btnDisconnect.TabIndex = 1;
+            btnDisconnect.Text = "Disconnect";
+            btnDisconnect.UseVisualStyleBackColor = true;
+            btnDisconnect.Click += btnDisconnect_Click;
+            // 
+            // textCaseFileName
+            // 
+            textCaseFileName.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            textCaseFileName.Location = new System.Drawing.Point(7, 24);
+            textCaseFileName.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            textCaseFileName.Name = "textCaseFileName";
+            textCaseFileName.Size = new System.Drawing.Size(584, 23);
+            textCaseFileName.TabIndex = 2;
+            textCaseFileName.Text = "C:\\Program Files (x86)\\PowerWorld\\Simulator15\\Sample Cases\\B7OPF.pwb";
+            // 
+            // btnOpenCase
+            // 
+            btnOpenCase.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            btnOpenCase.Location = new System.Drawing.Point(598, 22);
+            btnOpenCase.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            btnOpenCase.Name = "btnOpenCase";
+            btnOpenCase.Size = new System.Drawing.Size(88, 27);
+            btnOpenCase.TabIndex = 3;
+            btnOpenCase.Text = "Open Case";
+            btnOpenCase.UseVisualStyleBackColor = true;
+            btnOpenCase.Click += btnOpenCase_Click;
+            // 
+            // btnCloseCase
+            // 
+            btnCloseCase.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            btnCloseCase.Enabled = false;
+            btnCloseCase.Location = new System.Drawing.Point(693, 22);
+            btnCloseCase.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            btnCloseCase.Name = "btnCloseCase";
+            btnCloseCase.Size = new System.Drawing.Size(88, 27);
+            btnCloseCase.TabIndex = 4;
+            btnCloseCase.Text = "Close Case";
+            btnCloseCase.UseVisualStyleBackColor = true;
+            btnCloseCase.Click += btnCloseCase_Click;
+            // 
+            // btnGetGenParams
+            // 
+            btnGetGenParams.Enabled = false;
+            btnGetGenParams.Location = new System.Drawing.Point(7, 55);
+            btnGetGenParams.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            btnGetGenParams.Name = "btnGetGenParams";
+            btnGetGenParams.Size = new System.Drawing.Size(175, 27);
+            btnGetGenParams.TabIndex = 5;
+            btnGetGenParams.Text = "Get Gen Parameters";
+            btnGetGenParams.UseVisualStyleBackColor = true;
+            btnGetGenParams.Click += btnGetGenParams_Click;
+            // 
+            // btnOPF
+            // 
+            btnOPF.Enabled = false;
+            btnOPF.Location = new System.Drawing.Point(7, 89);
+            btnOPF.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            btnOPF.Name = "btnOPF";
+            btnOPF.Size = new System.Drawing.Size(175, 27);
+            btnOPF.TabIndex = 6;
+            btnOPF.Text = "OPF";
+            btnOPF.UseVisualStyleBackColor = true;
+            btnOPF.Click += btnOPF_Click;
+            // 
+            // btnScaleCase
+            // 
+            btnScaleCase.Enabled = false;
+            btnScaleCase.Location = new System.Drawing.Point(7, 122);
+            btnScaleCase.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            btnScaleCase.Name = "btnScaleCase";
+            btnScaleCase.Size = new System.Drawing.Size(175, 27);
+            btnScaleCase.TabIndex = 7;
+            btnScaleCase.Text = "Scale Case";
+            btnScaleCase.UseVisualStyleBackColor = true;
+            btnScaleCase.Click += btnScaleCase_Click;
+            // 
+            // btnSendGenToExcel
+            // 
+            btnSendGenToExcel.Enabled = false;
+            btnSendGenToExcel.Location = new System.Drawing.Point(7, 156);
+            btnSendGenToExcel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            btnSendGenToExcel.Name = "btnSendGenToExcel";
+            btnSendGenToExcel.Size = new System.Drawing.Size(175, 27);
+            btnSendGenToExcel.TabIndex = 8;
+            btnSendGenToExcel.Text = "Send Gen Info to Excel";
+            btnSendGenToExcel.UseVisualStyleBackColor = true;
+            btnSendGenToExcel.Click += btnSendGenToExcel_Click;
+            // 
+            // gbOperations
+            // 
+            gbOperations.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            gbOperations.Controls.Add(textLog);
+            gbOperations.Controls.Add(textCaseFileName);
+            gbOperations.Controls.Add(btnSendGenToExcel);
+            gbOperations.Controls.Add(btnOpenCase);
+            gbOperations.Controls.Add(btnScaleCase);
+            gbOperations.Controls.Add(btnCloseCase);
+            gbOperations.Controls.Add(btnOPF);
+            gbOperations.Controls.Add(btnGetGenParams);
+            gbOperations.Enabled = false;
+            gbOperations.Location = new System.Drawing.Point(14, 47);
+            gbOperations.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            gbOperations.Name = "gbOperations";
+            gbOperations.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            gbOperations.Size = new System.Drawing.Size(788, 383);
+            gbOperations.TabIndex = 9;
+            gbOperations.TabStop = false;
+            gbOperations.Text = "Operations";
+            // 
+            // textLog
+            // 
+            textLog.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            textLog.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            textLog.Location = new System.Drawing.Point(189, 58);
+            textLog.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            textLog.Multiline = true;
+            textLog.Name = "textLog";
+            textLog.ReadOnly = true;
+            textLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            textLog.Size = new System.Drawing.Size(591, 318);
+            textLog.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(816, 444);
+            Controls.Add(gbOperations);
+            Controls.Add(btnDisconnect);
+            Controls.Add(btnConnect);
+            Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Form1";
+            gbOperations.ResumeLayout(false);
+            gbOperations.PerformLayout();
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.TextBox textCaseFileName;
+        private System.Windows.Forms.Button btnOpenCase;
+        private System.Windows.Forms.Button btnCloseCase;
+        private System.Windows.Forms.Button btnGetGenParams;
+        private System.Windows.Forms.Button btnOPF;
+        private System.Windows.Forms.Button btnScaleCase;
+        private System.Windows.Forms.Button btnSendGenToExcel;
+        private System.Windows.Forms.GroupBox gbOperations;
+        private System.Windows.Forms.TextBox textLog;
+    }
+}
+
---
+++ SimAuto Example C# Core/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using pwrworld;
+
+namespace SimAuto_Example
+{
+    public partial class Form1 : Form
+    {
+        private ISimulatorAuto FSimAuto;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = false;
+            btnDisconnect.Enabled = true;
+            gbOperations.Enabled = true;
+            FSimAuto = new SimulatorAuto();
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = true;
+            btnDisconnect.Enabled = false;
+            gbOperations.Enabled = false;
+            Marshal.FinalReleaseComObject(FSimAuto);
+            FSimAuto = null;
+        }
+
+        private void btnOpenCase_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(textCaseFileName.Text))
+            {
+                dynamic output = FSimAuto.OpenCase(textCaseFileName.Text);
+                if (string.IsNullOrEmpty(output[0]))
+                {
+                    btnOpenCase.Enabled = false;
+                    btnCloseCase.Enabled = true;
+                    btnGetGenParams.Enabled = true;
+                    btnOPF.Enabled = true;
+                    btnScaleCase.Enabled = true;
+                    btnSendGenToExcel.Enabled = true;
+                    btnDisconnect.Enabled = false;
+                    textLog.Text = textLog.Text + Environment.NewLine + "Opened case successfully!" + Environment.NewLine;
+                }
+                else
+                {
+                    MessageBox.Show("Error opening case", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textLog.Text = textLog.Text + Environment.NewLine + output[0] + Environment.NewLine;
+                }
+            }
+            else
+            {
+                MessageBox.Show("File not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCloseCase_Click(object sender, EventArgs e)
+        {
+            btnOpenCase.Enabled = true;
+            btnCloseCase.Enabled = false;
+            btnGetGenParams.Enabled = false;
+            btnOPF.Enabled = false;
+            btnScaleCase.Enabled = false;
+            btnSendGenToExcel.Enabled = false;
+            btnDisconnect.Enabled = true;
+            FSimAuto.CloseCase();
+        }
+
+        private void btnGetGenParams_Click(object sender, EventArgs e)
+        {
+            object[] fieldarray = new object[] { "BusNum", "GenID", "GenStatus", "GenAGCAble", "GenMW", "GenMVR" };
+            dynamic output = FSimAuto.GetParametersMultipleElement("GEN", fieldarray, "");
+            if (string.IsNullOrEmpty(output[0]))
+            {
+                textLog.Text = textLog.Text + Environment.NewLine;
+                for (int i = 0; i < output[1][0].Length; i++)
+                    textLog.Text = textLog.Text + output[1][0][i] + '\t' + output[1][1][i] + '\t' +
+                         output[1][2][i] + '\t' + output[1][3][i] + '\t' + string.Format("{0,7:F1}", float.Parse(output[1][4][i])) +
+                         '\t' + string.Format("{0,7:F1}", float.Parse(output[1][5][i])) + Environment.NewLine;
+            }
+            else
+            {
+                MessageBox.Show("Error getting generator parameters", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textLog.Text = textLog.Text + Environment.NewLine + output[0] + Environment.NewLine;
+            }
+        }
+
+        private void btnOPF_Click(object sender, EventArgs e)
+        {
+            dynamic output = FSimAuto.ListOfDevices("GEN", "");
+            if (string.IsNullOrEmpty(output[0]))
+            {
+                object[] fieldlist = new object[] { "BusNum", "GenID", "GenAGCAble" };
+                object[] objects = new object[output[1][0].Length];
+
+                for (int i = 0; i < output[1][0].Length; i++)
+                    objects[i] = new object[] { output[1][0][i], output[1][1][i], "YES" };
+
+                output = FSimAuto.ChangeParametersMultipleElement("GEN", fieldlist, objects);
+                if (!string.IsNullOrEmpty(output[0]))
+                {
+                    MessageBox.Show("Error setting generator GenAGCAble", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textLog.Text = textLog.Text + Environment.NewLine + output[0] + Environment.NewLine;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error getting generator list", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textLog.Text = textLog.Text + Environment.NewLine + output[0] + Environment.NewLine;
+            }
+
+            output = FSimAuto.ListOfDevices("AREA", "");
+            if (string.IsNullOrEmpty(output[0]))
+            {
+                object[] fieldlist = new object[] { "AreaNum", "BGAGC", "OPFAreaLineLim" };
+                object[] objects = new object[output[1][0].Length];
+
+                for (int i = 0; i < output[1][0].Length; i++)
+                    objects[i] = new object[] { output[1][0][i], "OPF", "YES" };
+
+                output = FSimAuto.ChangeParametersMultipleElement("AREA", fieldlist, objects);
+                if (!string.IsNullOrEmpty(output[0]))
+                {
+                    MessageBox.Show("Error setting area BGAGC and OPFAreaLineLim", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textLog.Text = textLog.Text + Environment.NewLine + output[0] + Environment.NewLine;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error getting area list", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textLog.Text = textLog.Text + Environment.NewLine + output[0] + Environment.NewLine;
+            }
+
+            output = FSimAuto.RunScriptCommand("Entermode(PowerFlow); SolvePowerFlow(DC); SolvePrimalLP; SolveFullSCOPF(OPF);");
+            if (string.IsNullOrEmpty(output[0]))
+                textLog.Text = textLog.Text + Environment.NewLine + "OPF executed successfully" + Environment.NewLine;
+            else
+                textLog.Text = textLog.Text + Environment.NewLine + output[0] + Environment.NewLine;
+
+            object[] fieldarray = new object[] { "BusNum", "BusMCMW" };
+            output = FSimAuto.GetParametersMultipleElement("BUS", fieldarray, "");
+            if (string.IsNullOrEmpty(output[0]))
+            {
+                textLog.Text = textLog.Text + Environment.NewLine;
+                for (int i = 0; i < output[1][0].Length; i++)
+                    textLog.Text = textLog.Text + output[1][0][i] + '\t' + string.Format("{0,7:F1}", float.Parse(output[1][1][i])) +
+                        Environment.NewLine;
+            }
+            else
+            {
+                MessageBox.Show("Error getting bus parameters", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textLog.Text = textLog.Text + Environment.NewLine + output[0] + Environment.NewLine;
+            }
+        }
+
+        private void btnScaleCase_Click(object sender, EventArgs e)
+        {
+            dynamic output = FSimAuto.ListOfDevices("AREA", "");
+            if (string.IsNullOrEmpty(output[0]))
+            {
+                object[] fieldlist = new object[] { "AreaNum", "BGScale" };
+                object[] objects = new object[output[1][0].Length];
+
+                for (int i = 0; i < output[1][0].Length; i++)
+                    objects[i] = new object[] { output[1][0][i], "YES" };
+
+                output = FSimAuto.ChangeParametersMultipleElement("AREA", fieldlist, objects);
+                if (!string.IsNullOrEmpty(output[0]))
+                {
+                    MessageBox.Show("Error setting area BGScale", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textLog.Text = textLog.Text + Environment.NewLine + output[0] + Environment.NewLine;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error getting area list", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textLog.Text = textLog.Text + Environment.NewLine + output[0] + Environment.NewLine;
+            }
+
+            output = FSimAuto.RunScriptCommand("Entermode(Edit); Scale(LOAD, FACTOR, [0.9], AREA);");
+            if (string.IsNullOrEmpty(output[0]))
+                textLog.Text = textLog.Text + Environment.NewLine + "Case successfully scaled by 0.9" + Environment.NewLine;
+            else
+                textLog.Text = textLog.Text + Environment.NewLine + output[0] + Environment.NewLine;
+        }
+
+        private void btnSendGenToExcel_Click(object sender, EventArgs e)
+        {
+            object[] fieldlist = new object[] { "BusNum", "GenID", "GenMW" };
+            dynamic output = FSimAuto.SendToExcel("GEN", "", fieldlist);
+            if (!string.IsNullOrEmpty(output[0]))
+            {
+                MessageBox.Show("Error sending data to Excel", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textLog.Text = textLog.Text + Environment.NewLine + output[0] + Environment.NewLine;
+            }
+        }
+    }
+}
